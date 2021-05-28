fe771e4f6916619923dbcabe3a2b4d25626f0527
Nicholas Boers <12831677+boersn@users.noreply.github.com>
2021-05-28 14:08:53 -0600

    Add hostname to output

--- src/main.c
+++ src/main.c
@@ -1,9 +1,12 @@
+#define _POSIX_C_SOURCE 200112L
+
 // https://stackoverflow.com/questions/5713451/is-it-safe-to-parse-a-proc-file
 // https://stackoverflow.com/questions/62126052/setvbuf-not-changing-buffer-size
 
+#include <bits/posix1_lim.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
 #include <unistd.h>
 
 #include "signals.h"
@@ -12,6 +15,7 @@
 #include "mem_stats.h"
 #include "net_stats.h"
 
+static char hostname[_POSIX_HOST_NAME_MAX + 1];
 static bool received_timer_signal = false;
 static bool received_intr_signal = false;
 
@@ -20,18 +24,26 @@ int main ()
   register_intr_signal_handler (&received_intr_signal);
   register_timer_signal_handler (&received_timer_signal);
 
+  int rc = gethostname (hostname, _POSIX_HOST_NAME_MAX + 1);
+  if (rc != 0)
+    {
+      perror ("gethostname");
+      exit (EXIT_FAILURE);
+    }
+
   initialize_cpu_state ();
   initialize_disk_state ();
   initialize_mem_state ();
   initialize_net_state ();
 
-  printf ("%s,%s,%s,%s\n",
+  printf ("%s,%s,%s,%s,%s\n",
+	  "hostname",
           format_cpu_stats (NULL, ","),
           format_disk_stats (NULL, ","),
           format_mem_stats (NULL, ","),
           format_net_stats (NULL, ","));
 
-  create_and_start_timer (500);
+  create_and_start_timer (10);
 
   refresh_cpu_state ();
   refresh_disk_state ();
@@ -54,7 +66,8 @@ int main ()
           mem_stats mem_stats = get_mem_stats ();
           net_stats net_stats = get_net_stats ();
 
-          printf ("%s,%s,%s,%s\n",
+          printf ("%s,%s,%s,%s,%s\n",
+		  hostname,
                   format_cpu_stats (&cpu_stats, ","),
                   format_disk_stats (&disk_stats, ","),
                   format_mem_stats (&mem_stats, ","),
@@ -74,5 +87,5 @@ int main ()
   cleanup_mem_state ();
   cleanup_net_state ();
 
-  exit (0);
+  exit (EXIT_SUCCESS);
 }
