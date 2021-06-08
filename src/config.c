17d179c426ab38e1d26df17967665c4ea91d8219
Nicholas Boers <12831677+boersn@users.noreply.github.com>
2021-06-07 21:28:39 -0600

    Change fstat default delay to match virt-top usage

--- src/config.c
+++ src/config.c
@@ -6,7 +6,7 @@
 config create_config(char *program_name)
 {
   return (config){
-    .delay_seconds = 1.0,
+    .delay_seconds = 0.1,
     .program_name = basename(program_name),
     .quiet = false,
     .timestamp = false,
